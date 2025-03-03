3c65506f1fac5a12b5beae376068d1bdf0ee6a6f
Ahmed Alshatebi <154013344+a7med-alshatebi@users.noreply.github.com>
2025-03-04 00:05:55 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{index.html,about.html,cont.html,serv.html}",
   ],
   theme: {
     extend: {},
