3d6ab6a19c7571bd3bef83998d024ce5df57cc0d
Ahmed Alshatebi <ahmedalshatibi22@gmail.com>
2025-03-03 23:33:30 +0300

    ss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}", "./index.html","./serv.html","./src/**/*.{jsx,ts,tsx,vue}" ,"./src/**/*.{css,scss,sass}", "./about.html","./cont.html"],
-    theme: {
-      extend: {
-        'tablet': '640px',
-        // => @media (min-width: 640px) { ... }
-  
-        'laptop': '1024px',
-        // => @media (min-width: 1024px) { ... }
-  
-        'desktop': '1280px',
-        // => @media (min-width: 1280px) 
-  
-        'telephone': '375px',
-        // => @media (min-width: 375px) { ... }
-      
-        listStyleImage: {
-          checkmark: 'url("/img/checkmark.png")', 
-      },
-      backgroundImage: {
-        'hero-pattern': "url('src/socatra.jpg')",
-        'footer-texture': "url('/img/footer-texture.png')",
-      },
-    },
-    plugins: [],
-  }
-  }
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
